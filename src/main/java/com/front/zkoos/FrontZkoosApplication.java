6f901576a61f2967bfd4b3bbff2312c514032172
Higinio2723 <52228319+Higinio2723@users.noreply.github.com>
2023-01-31 19:24:23 -0600

    Realizando configuracion para el proceso de avanzar entre formularios

--- src/main/java/com/front/zkoos/FrontZkoosApplication.java
+++ src/main/java/com/front/zkoos/FrontZkoosApplication.java
@@ -2,8 +2,10 @@ package com.front.zkoos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.front.zkoos.*"})
 public class FrontZkoosApplication {
 
 	public static void main(String[] args) {

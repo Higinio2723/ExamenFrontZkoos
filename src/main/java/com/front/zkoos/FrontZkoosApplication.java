fd84bd51e8c5e89b19d21d1043639dc7a2b70de7
Higinio2723 <52228319+Higinio2723@users.noreply.github.com>
2023-01-31 18:43:41 -0600

    Se realiza cambio de nombre de paquetes

--- src/main/java/com/front/zkoos/FrontZkoosApplication.java
+++ src/main/java/com/front/zkoos/FrontZkoosApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.front.zkoos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FrontZkoosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FrontZkoosApplication.class, args);
 	}
 
 }
